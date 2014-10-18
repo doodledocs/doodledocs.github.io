a848cc441c9c0875fb33bf321c51a29bc483c60d
Gino Clement <ginoclement@gmail.com>
2014-10-18 05:41:36 -0700

    Random crap lol

--- js/tools.js
+++ js/tools.js
@@ -1,18 +1,51 @@
-function makeRect(layer, left, top, width, height, fill, angle, opacity){
-    angle = typeof angle !== 'undefined' ? angle : 0;
-    fill = typeof fill !== 'undefined' ? fill : null;
-    opacity = typeof opacity !== 'undefined' ? opacity : 1.0;
+function getOptions(){
+    var ab = $("#actionbar");
+    options = {};
+    if($('[name="size"]').val() > 0){
+        options["size"] = $('[name="size"]').val();
+        alert($('[name="size"]').val());
+    }
+    options["opacity"] = ($("#color_type").val() == "None") ? 0.0 : $('[name="opacity"]').val() / 100.0;
+    options["angle"] = $('[name="angle"]').val();
+    //Color settings
+    if($("#color_type").val() == "Gradient"){
+        alert("Need to implement Gradient (tools.js)");
+    }
+    if($("#color_type").val() == "Fill"){
+        options["fill"] = $("#colorpicker").val();
+        console.log($("#colorpicker").val());
+    }
+    if($("#selected_shape").val() == "Circle"){
+        options["radius"] = $('[name="radius"]').val();
+    } else if($("#selected_shape").val() == "Triangle" || $("#selected_shape").val() == "Rectangle"){
+        options["height"] = $('[name="height"]').val();
+        options["width"] = $('[name="width"]').val();
+    }
+
+    console.log(options);
+    return options;
+}
+
+function makeShape(layer){
+    // console.log($("#selected_shape").val());
+    switch($("#selected_shape").val()){
+        case "Circle":
+            makeCircle(layer, getOptions());
+            break;
+        case "Triangle":
+            makeTriangle(layer, getOptions());
+            break;
+        case "Rectangle":
+            makeRect(layer, getOptions());
+    }
+}
 
 
-    var rect = new fabric.Rect({
-        left: left,
-        top: top,
-        fill: fill,
-        width: width,
-        height: height,
-        angle: angle,
-        opacity: opacity
-    });
+function makeRect(layer, options){
+    options["left"] = 200;
+    options["top"] = 100;
+    console.log(options);
+    var rect = new fabric.Rect(options);
     layer.add(rect);
     setEvents(rect);
 }
@@ -26,37 +59,20 @@ function setEvents(object) {
 	}
 }
 
-function makeCircle(layer, left, top, radius, fill, angle, opacity){
-    angle = typeof angle !== 'undefined' ? angle : 0;
-    fill = typeof fill !== 'undefined' ? fill : null;
-    opacity = typeof opacity !== 'undefined' ? opacity : 1.0;
-
-    var circle = new fabric.Circle({
-        left: left,
-        top: top,
-        fill: fill,
-        radius: radius,
-        angle: angle,
-        opacity: opacity
-    });
+function makeCircle(layer, options){
+    options["left"] = 100;
+    options["top"] = 50;
+    console.log(options);
+    var circle = new fabric.Circle(options);
     layer.add(circle);
     setEvents(circle);
 }
 
-function makeTriangle(layer, left, top, width, height, fill, angle, opacity){
-    angle = typeof angle !== 'undefined' ? angle : 0;
-    fill = typeof fill !== 'undefined' ? fill : null;
-    opacity = typeof opacity !== 'undefined' ? opacity : 1.0;
-    
-    var triangle = new fabric.Triangle({
-        left: left,
-        top: top,
-        fill: fill,
-        width: width,
-        height: height,
-        angle: angle,
-        opacity: opacity
-    });
+function makeTriangle(layer, options){
+    options["left"] = 200;
+    options["top"] = 50;
+    console.log(options);
+    var triangle = new fabric.Triangle(options);
     layer.add(triangle);
     setEvents(triangle);
 }
